ba0eb5b09bdbc1ec2e3e617b26739471214ace9e
Jun Yan Gan <Jun-Yan.Gan@georgebrown.ca>
2021-11-30 17:48:26 -0500

    Adding template slowly

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import logo from './logo.svg';
+import React from 'react'
+import axios from 'axios'
 import './App.css';
+import './index.css'
+
+const api = {
+  key: "669c77631b8568499d0c85579429ce59",
+  base: "http://api.openweathermap.org/data/2.5/"
+}
 
 function App() {
   return (
-    <div>
-
+    <div className="app">
+      <main>
+        <div className="search-box">
+          <input 
+            type="text"
+            className="search-bar"
+            placeholder="Search..."
+          />
+        </div>
+      </main>
     </div>
   );
 }
