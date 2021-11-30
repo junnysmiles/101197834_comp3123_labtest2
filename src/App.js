96843d4967e75e621da1f0fe06d7fce8c83143a8
Jun Yan Gan <Jun-Yan.Gan@georgebrown.ca>
2021-11-30 18:06:03 -0500

    Adding more style template

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ const api = {
 }
 
 function App() {
+
+  let today = new Date().toDateString();
+
   return (
     <div className="app">
       <main>
@@ -18,6 +21,16 @@ function App() {
             className="search-bar"
             placeholder="Search..."
           />
+        </div>
+        <div className="location-box">
+          <div className="location">Toronto, ON</div>
+          <div className="date">{today}</div>
+        </div>
+        <div className="weather-box">
+          <div className="temp">
+            15°c
+          </div>
+          <div className="weather">Sunny</div>
         </div>
       </main>
     </div>
